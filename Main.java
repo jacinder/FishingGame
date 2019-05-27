35c77dfbc369cab9de54aacce225aa907b64778b
Nariniiii <21800001@handong.edu>
2019-05-27 11:08:24 +0900

    메인 완전 좀만 추가함

--- Main.java
+++ Main.java
@@ -3,8 +3,13 @@ import java.util.Timer;
 import java.util.TimerTask;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Fishing Fishing Happy Fishing~");
-    System.out.println("gun ha");
-    // dddd
+    String userName;
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("당신의 이름은?");
+    userName = keyboard.nextLine();
+    System.out.println("한동대학교 전산전자공학부 " + userName + "은 부족한 학비를 모으기 위해 낚시를 시작하게 되는데...");
+
+
+
   }
 }
