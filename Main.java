26eecf4e82fb8b503032ec7d545b074ee0380bc8
Nariniiii <21800001@handong.edu>
2019-06-06 00:40:37 +0900

    배경음악 추가(언더더씨~~)

--- Main.java
+++ Main.java
@@ -16,11 +16,26 @@ class Main{
       String[] fishArray = {"Salmon", "Flatfish", "Squid", "Octopus", "Minnow", "Shrimp", "Carp", "Tuna", "Mackerel", "Saury"};
       Random random = new Random();
       loadFile file = new loadFile();
+      Sound("UTSS.wav", true);
       new Intro(fishArray);
    }
    public static void setUserName(String userName) {
       Main.userName = userName;
    }
+   public static void Sound(String file, boolean Loop){
+		Clip clip;
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(file)));
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+			clip.start();
+
+			if (Loop) clip.loop(-1);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
 }
 class loadFile{
    public loadFile(){
